1436cdeea73df2a0ca5ffb6557f8c177d1df3afe
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2020-12-07 09:33:31 +0300

    feat: parse md in modals

--- src/components/ConfirmModal/ConfirmModal.tsx
+++ src/components/ConfirmModal/ConfirmModal.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+
+import { parseMD } from "../../utils/parseMD";
 
 interface ConfirmModalProps {
   title: string;
@@ -34,7 +35,7 @@ const ConfirmModal = (props: ConfirmModalProps) => {
 
       <DialogContent>
         <DialogContentText id="alert-dialog-description">
-          {description}
+          {parseMD(description)}
         </DialogContentText>
       </DialogContent>
 
